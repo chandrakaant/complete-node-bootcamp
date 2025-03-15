b19270d6adb7b2c4ccdc51354d00b7e5c5d309f5
chandrakantfuture <chandrakant.dev.future@gmail.com>
2025-03-15 17:27:37 +0530

    Non blocking asynchronous callback functions

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,8 +1,26 @@
 const fs = require('fs');
 
- const textin = fs.readFileSync('./txt/input.txt',  'utf-8');
+//Blocking sync way
+const textin = fs.readFileSync('./txt/input.txt',  'utf-8');
 console.log(textin);
 
 const textout = `This is what we know about the avaocado: ${textin}.\nCreated on ${Date.now()}`;
 fs.writeFileSync('./txt/output.txt', textout);
 console.log("File written");
+
+//non- blocking async way - this is a call back function
+//the file is getting read in background, as soon as its done, it calls the callback function with error and data
+//error is always the first parameter in node js
+fs.readFile('./txt/start.txt','utf-8', (err, data1)=> {
+    if(err) return console.log('Error')
+    fs.readFile(`./txt/${data1}.txt`,'utf-8', (err, data2)=> {
+        console.log(data2);
+        fs.readFile(`./txt/append.txt`,'utf-8', (err, data3)=> {
+            console.log(data3);
+            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`,'utf-8', err => {
+                console.log('Your file has been written');
+            })
+        })
+    })
+})
+console.log('Will read file');
